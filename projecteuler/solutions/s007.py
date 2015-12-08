cd894a70d79fa14d8e29b4d693722b234cf8f78e
Robert Stratton <r.a.stratton88@gmail.com>
2015-12-08 05:49:57 -0800

    Add solution and answer for problem 007

---
+++ projecteuler/solutions/s007.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+PROBLEM: By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
+see that the 6th prime is 13.  What is the 10 001st prime number?
+"""
+
+
+from projecteuler.numbers import primes
+
+def solve():
+    return primes[10000]
+
+
+if __name__ == "__main__":
+    print(solve())
