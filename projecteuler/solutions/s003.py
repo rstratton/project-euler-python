d5f02325a044c814d17c518783d0dcd3be63c24f
Robert Stratton <r.a.stratton88@gmail.com>
2015-12-27 07:21:43 -0800

    Use large prime factor function for problem 003

--- projecteuler/solutions/s003.py
+++ projecteuler/solutions/s003.py
@@ -5,11 +5,11 @@ PROBLEM: What is the largest prime factor of the number 600851475143?
 """
 
 
-from projecteuler.util import prime_factors
+from projecteuler.util import prime_factors_large_n
 
 
 def solve():
-    return max(prime_factors(600851475143))
+    return max(prime_factors_large_n(600851475143))
 
 
 if __name__ == "__main__":
